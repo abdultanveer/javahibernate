56ae6a93b230abc22dff7656efa0debaa4b80809
abdul <abdul.tanveer@gmail.com>
2021-04-30 11:41:27 +0530

    initial commit

---
+++ JavaHibernate/src/main/java/com/niit/JavaHibernate/App.java
@@ -0,0 +1,29 @@
+package com.niit.JavaHibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Student student = new Student(11, "yashas", 1,11);
+        
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+        
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        
+        session.save(student);
+        transaction.commit();
+      
+    }
+}
